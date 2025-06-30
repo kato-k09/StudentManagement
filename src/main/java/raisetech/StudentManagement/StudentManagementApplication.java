95e5f02c86d541ec52348c2229908ae3f95e9324
kato-k09 <kazuki.1@royal.ocn.ne.jp>
2025-06-30 15:37:46 +0900

    18_DBを使わないWebアプリケーション構築　課題
    student Mapで学生情報を管理する機能を実装
    
    学生情報登録　例
    curl.exe -X POST -d 'name=sato&age=25' http://localhost:8080/setStudentMapInfo
    
    学生情報出力　例
    curl.exe -X POST -d 'name=sato' http://localhost:8080/getStudentMapInfo

--- src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
@@ -1,20 +1,54 @@
 package raisetech.StudentManagement;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class StudentManagementApplication {
 
+	private String name = "Enami Kouji";
+	private String age = "37";
+
+	private Map<String, String> student = new HashMap<>();
+
 	public static void main(String[] args) {
 		SpringApplication.run(StudentManagementApplication.class, args);
 	}
 
-	@GetMapping("/hello")
-	public String hello() {
-		return "Hello, World!";
+	@GetMapping("/studentInfo")
+	public String getStudentInfo() {
+		return name + " " + age + "歳";
+	}
+	@PostMapping("/studentInfo")
+	public void setStudentInfo(String name, String age) {
+		this.name = name;
+		this.age = age;
+	}
+	@PostMapping("/studentName")
+	public void updateStudentName(String name) {
+		this.name = name;
+	}
+
+
+	//student Mapで学生情報を管理
+	@PostMapping("/getStudentMapInfo")
+	public String getStudentMapInfo(String name) {
+		return student.entrySet().stream()
+				.filter(v -> v.getKey().equals(name))
+				.map(v -> "氏名: " + v.getKey() + " 年齢: " + v.getValue())
+				.findFirst()
+				.orElse("該当者なし");
+
+	}
+	@PostMapping("/setStudentMapInfo")
+	public void setStudentMapInfo(String name, String age) {
+		this.student.put(name,age);
 	}
 }
